3536e22841dd8e11155f388090367544138e3ef3
awhitbeck <whitbeck.andrew@gmail.com>
2022-06-20 18:40:14 -0500

    initial commit

---
+++ tictactoe/PlayRLAgent.py
@@ -0,0 +1,17 @@
+from RLAgent import *
+from tictactoe import *
+
+#qfunc_500_2022-06-18_15:33:50.160950.pkl
+#vfunc_500_2022-06-18_15:33:50.171808.pkl
+
+a = RLAgent(Game(None))
+a.load(vfunc_file_name="Models/vfunc_500_2022-06-18_15:33:50.171808.pkl",
+       qfunc_file_name="Models/qfunc_500_2022-06-18_15:33:50.160950.pkl"
+       )
+a.lambda_=0 ### make sure that AI is always play using its policy
+a.playing_x=False
+g = Game(a)
+g.interactive_play()
+
+
+

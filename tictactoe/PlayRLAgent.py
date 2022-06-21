874526a91e38acaff7c53dd71dfd6b83ee787fb7
awhitbeck <whitbeck.andrew@gmail.com>
2022-06-21 14:42:47 -0500

    updating to the latest training

--- tictactoe/PlayRLAgent.py
+++ tictactoe/PlayRLAgent.py
@@ -5,8 +5,8 @@ from tictactoe import *
 #vfunc_500_2022-06-18_15:33:50.171808.pkl
 
 a = RLAgent(Game(None))
-a.load(vfunc_file_name="Models/vfunc_500_2022-06-18_15:33:50.171808.pkl",
-       qfunc_file_name="Models/qfunc_500_2022-06-18_15:33:50.160950.pkl"
+a.load(vfunc_file_name="Models/vfunc_500_2022-06-20_19:12:23.006871.pkl",
+       qfunc_file_name="Models/qfunc_500_2022-06-20_19:12:22.994413.pkl"
        )
 a.lambda_=0 ### make sure that AI is always play using its policy
 a.playing_x=False
